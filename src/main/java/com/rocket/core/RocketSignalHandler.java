0c02d361b8d8ae56cb81ba09d775fcf26b7dfea9
Narender Singh <bknarendersingh@gmail.com>
2018-07-11 18:28:37 +0530

    Adding External singal handler in Rocket

---
+++ src/main/java/com/rocket/core/RocketSignalHandler.java
@@ -0,0 +1,14 @@
+package com.rocket.core;
+
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+public class RocketSignalHandler implements SignalHandler {
+
+	@Override
+	public void handle(Signal sig) {
+
+		
+	}
+
+}
