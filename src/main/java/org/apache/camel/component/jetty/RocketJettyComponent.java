c74d13d48e6f2c4a02f5e3565ea5f9a17819ff76
Narender Singh <bknarendersingh@gmail.com>
2018-07-24 17:12:23 +0530

    Added Custom jetty comp

--- src/main/java/org/apache/camel/component/jetty/RocketJettyComponent.java
+++ src/main/java/org/apache/camel/component/jetty/RocketJettyComponent.java
@@ -1,7 +1,18 @@
 package org.apache.camel.component.jetty;
 
 import org.apache.camel.component.jetty9.JettyHttpComponent9;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RocketJettyComponent extends JettyHttpComponent9 {
+
+	private static final Logger l = LoggerFactory.getLogger(RocketJettyComponent.class);
+
+	private static final int DEFAULT_THREAD_COUNT;
+
+	static {
+		DEFAULT_THREAD_COUNT = 1000;
+		l.info("Default Thread Count of RocketJettyComponent is {}", DEFAULT_THREAD_COUNT);
+	}
 
 }
