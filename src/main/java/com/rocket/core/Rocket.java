b8a816deb0c0bcd848e3f491ec49e62cc553be15
Narender Singh <bknarendersingh@gmail.com>
2018-07-24 17:43:44 +0530

    updating config

--- src/main/java/com/rocket/core/Rocket.java
+++ src/main/java/com/rocket/core/Rocket.java
@@ -8,8 +8,6 @@ import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Future;
-import java.util.function.Supplier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
